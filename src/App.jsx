368604ae52ca8f1834484054211ccb05850788e7
Forgács Szabolcs <30867971+fszabi@users.noreply.github.com>
2023-12-02 09:28:42 +0100

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,7 @@
+export default function App() {
+  return (
+    <div className="container h-screen grid place-content-center text-center">
+      <h1 className="text-4xl font-bold">Vite React Template</h1>
+    </div>
+  );
+}
---
+++ tailwind.config.js
@@ -0,0 +1,32 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    screens: {
+      sm: "35em",
+      md: "50em",
+      lg: "65em",
+      xl: "80em",
+    },
+    fontSize: {
+      sm: ["0.875rem", "1.5"],
+      base: ["1rem", "1.5"],
+      lg: ["1.25rem", "1.4"],
+      xl: ["clamp(1.56rem, 0.79vw + 1.36rem, 2rem)", "1.3"],
+      "2xl": ["clamp(1.95rem, 1.29vw + 1.63rem, 2.66rem)", "1.3"],
+      "3xl": ["clamp(2.44rem, 2.02vw + 1.94rem, 3.55rem)", "1.2"],
+      "4xl": ["clamp(3.05rem, 3.06vw + 2.29rem, 4.73rem)", "1.2"],
+    },
+    extend: {
+      colors: {
+        primary: {
+          400: "#ffaf02",
+        },
+      },
+      fontFamily: {
+        sans: ["Poppins", "system-ui", "sans-serif"],
+      },
+    },
+  },
+  plugins: [],
+};
