af556779d2ef1a2bc98b73e4ea6d60e12e6057e4
Forgacs Szabolcs <forgacsszabolcs@pm.me>
2023-12-13 14:17:45 +0100

    font changes

--- src/routes/Contact.jsx
+++ src/routes/Contact.jsx
@@ -12,7 +12,7 @@ export default function Contact() {
     <main>
       <Section>
         <Container>
-          <h1 className="uppercase italic font-bold text-3xl text-center">
+          <h1 className="uppercase italic font-bold text-3xl max-sm:text-2xl text-center">
             Kapcsolatfelvétel
           </h1>
           <Form />
