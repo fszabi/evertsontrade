98c0c2fb6af47d5857a7ec23a1ae0340c987de02
Forgacs Szabolcs <forgacsszabolcs@pm.me>
2023-12-25 11:19:33 +0100

    changed text

--- src/components/TradeGuideList.jsx
+++ src/components/TradeGuideList.jsx
@@ -11,7 +11,7 @@ const tradeGuideTexts = {
   fifthTradeGuideText: `Kereskedők általában buff 100-110% körül adnak el és buff 80-95% között vásárolnak. 
   Szintén függ a vétel és az eladási ár is az adott item float,sticker+pozició,pattern, liquiditás és egyéb tényezőktől.`,
   sixthTradeGuideText: `Ez egy folyamatosan hullámzó piac, nagy valószinűséggel bukni fogsz egyszerű játékosként minden skinen ezért 
-  csak akkor vásárolj skineket ha nem zavar hogy 5-10%ot vagy akár nagyobb %os bukóbanl tudod eladni. Ezzel számolnod kell 
+  csak akkor vásárolj skineket ha nem zavar hogy 5-10%ot vagy akár nagyobb %os bukóban tudod eladni. Ezzel számolnod kell 
   mivel alapjáraton a steam piac is 12%ot számit fel ami buff ár szerint 5-8% lehet.`,
   seventhTradeGuideText: `Illetve vannak olyan itemek amiket nem szabad vásárolni kivéve ha te direkt olyat szeretnél. 
   Pl: Statrakos kések vagy minimal wear dopplerek,tiger/slaugherek esetleg souvenir skinek vagy ww bs itemek.`,
