070a63869a5ca39d19d575ceb56a34cb1a66699f
Forgacs Szabolcs <forgacsszabolcs@pm.me>
2024-03-28 21:09:50 +0100

    added cs2 betting patreon

--- src/components/ServiceCard.jsx
+++ src/components/ServiceCard.jsx
@@ -1,14 +1,15 @@
-export default function ServiceCard({ title, body, href }) {
+export default function ServiceCard({ title, body, href, hrefText, video }) {
   return (
     <div className="bg-neutral-900 p-7 rounded-xl shadow-2xl space-y-5">
       <h3 className="text-blue-400 uppercase text-xl max-sm:text-lg">
         {title}
       </h3>
       <p className="leading-relaxed max-sm:text-sm">{body}</p>
-      {href && (
-        <div className="space-y-10">
+
+      <div className="space-y-10">
+        {href && (
           <p className="leading-relaxed max-sm:text-sm">
-            A tanfolyam az alábbi linken érhető el:
+            {hrefText}
             <a
               className="text-blue-400 underline break-all block"
               target="_blank"
@@ -17,17 +18,19 @@ export default function ServiceCard({ title, body, href }) {
               {href}
             </a>
           </p>
+        )}
+        {video && (
           <iframe
             className="mx-auto rounded-lg w-full h-[50vh]"
-            src="https://www.youtube.com/embed/SR4p4JyFRVQ?si=iaJeGORjzuySgzLM"
+            src={video}
             title="YouTube video player"
             frameborder="0"
             allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
             referrerpolicy="strict-origin-when-cross-origin"
             allowfullscreen
           ></iframe>
-        </div>
-      )}
+        )}
+      </div>
     </div>
   );
 }
--- src/routes/Services.jsx
+++ src/routes/Services.jsx
@@ -7,6 +7,7 @@ const cardBodies = {
   Képzeld el azt, hogy ameddig te alszol addig a játéktárgyaid pénzt termelnek neked! Hihetetlennek hangzik igaz? 
   Elárulom egyáltalán nem az. Minél többet alszok annál több pénzem lesz, mivel az idő csak nekem dolgozik. 
   Ha szeretnél játszva pénzt keresni csak abban az esetben keress mivel az élet egy játék és én a legmagasabb szinten játszok!`,
+  bettingBody: `Készülőben Magyarország első CS2 fogadási tippeket adó csoportja! Profi elemzői háttérrel vágok bele. Április 7.-én már indulni fog a csoport mivel kezdődik az IEM Chengdu.`,
   middlemanBody: `Amennyiben vásárolni vagy skint szeretnél eladni más személynek de nem tudtok dűlőre jutni, hogy ki adjon 
   előre szivesen vállalom a közvetitői szerepet. A közvetités menete: Mindenféleképpen az eladó/vevő előre el kell utalja
    a pénzt nekem, majd ha hozzám került a pénz,az adott item gazdát cserél és ezek után fogom elutalni az eladónak/vevőnek
@@ -31,6 +32,15 @@ export default function Services() {
             title="CS2 Tanfolyam"
             body={cardBodies.courseBody}
             href="https://whop.com/checkout/d00QLTqFUrW7xpGsF-v2wR-TSy7-GYCN-EJZrfsBnual6/"
+            hrefText="A tanfolyam az alábbi linken érhető el:"
+            video="https://www.youtube.com/embed/SR4p4JyFRVQ?si=iaJeGORjzuySgzLM"
+          />
+
+          <ServiceCard
+            title="CS2 Fogadás Csoport"
+            body={cardBodies.bettingBody}
+            href="https://www.patreon.com/eVERTSONBETS"
+            hrefText="A csoport az alábbi linken érhető el:"
           />
 
           <ServiceCard
